8ad6624e05db0320350290fb125ae6b3f16cf216
user <kavkras83@mail.ru>
2021-07-05 21:17:59 +0300

    refactoring: new project_Scopes

--- SpringApp_Scopes/src/main/java/ru/krasilnikov/spring/TestSpring.java
+++ SpringApp_Scopes/src/main/java/ru/krasilnikov/spring/TestSpring.java
@@ -10,17 +10,26 @@ public class TestSpring {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context =
 			new ClassPathXmlApplicationContext("applicationContext.xml");
-//		Music music = context.getBean("musicBean", Music.class);
 
-		// внедрение зависимостей вручную
-//		MusicPlayer musicPlayer = new MusicPlayer(music);
+		MusicPlayer firstmMsicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+		MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+// проверка указания объектов на один и тот же участок памяти
+		// тем самым убедимся что по умолчанию Singelton запускает
+		boolean comparison = firstmMsicPlayer == secondMusicPlayer;
 
-		// после внедрения зависимости можно сразу использовать музыкальный плеер
-		MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-		musicPlayer.playMusik();
 
-		System.out.println(musicPlayer.getName());
-		System.out.println(musicPlayer.getVolume());
+		System.out.println(comparison);
+
+		System.out.println(firstmMsicPlayer);
+		System.out.println(secondMusicPlayer);
+// при установке громкости на первом объекте, у второго установится такая же громкость
+		firstmMsicPlayer.setVolume(10);
+
+		System.out.println(firstmMsicPlayer.getVolume());
+		System.out.println(secondMusicPlayer.getVolume());
+		//
+//		System.out.println(musicPlayer.getName());
+//		System.out.println(musicPlayer.getVolume());
 		context.close();
 	}
 }
