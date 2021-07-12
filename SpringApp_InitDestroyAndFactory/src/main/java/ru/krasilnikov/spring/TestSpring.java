a7e036a8ff44ebac94ff8d80678c9d10ae8d45d5
user <kavkras83@mail.ru>
2021-07-12 17:15:58 +0300

    refactoring: пробы с destroy_SpringApp_InitDestroyAndFactory

--- SpringApp_InitDestroyAndFactory/src/main/java/ru/krasilnikov/spring/TestSpring.java
+++ SpringApp_InitDestroyAndFactory/src/main/java/ru/krasilnikov/spring/TestSpring.java
@@ -29,10 +29,10 @@ public class TestSpring {
 //		System.out.println(secondMusicPlayer);
 // при установке громкости на первом объекте, у второго установится такая же громкость
 //		firstmMsicPlayer.setVolume(10);
-//
+////
 //		System.out.println(firstmMsicPlayer.getVolume());
 //		System.out.println(secondMusicPlayer.getVolume());
-//		//
+
 //		System.out.println(musicPlayer.getName());
 //		System.out.println(musicPlayer.getVolume());
 		context.close();
