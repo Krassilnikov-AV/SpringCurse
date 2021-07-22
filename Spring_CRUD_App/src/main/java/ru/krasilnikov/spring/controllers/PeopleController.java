2d578b4ce0565d9a25deb4789f42493d2320978b
user <kavkras83@mail.ru>
2021-07-22 15:53:49 +0300

    refactoring: start project

---
+++ Spring_CRUD_App/src/main/java/ru/krasilnikov/spring/controllers/PeopleController.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021 Tander, All Rights Reserved.
+ */
+
+package ru.krasilnikov.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.krasilnikov.spring.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+
+	private final PersonDAO personDAO;
+
+	public PeopleController(PersonDAO personDAO) {
+		this.personDAO=personDAO;
+	}
+
+	@GetMapping()
+	public String index(Model model) {
+		// получим всех людей из DAO и передадим на отображение в представлении
+		model.addAttribute("people", personDAO.index());
+		return  "people/index";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable("id") int id, Model model) {
+// получим одного человека по id из DAO и передадим на отображение в представление
+		model.addAttribute("person", personDAO.show(id));
+
+		return "people/show";
+	}
+
+}
