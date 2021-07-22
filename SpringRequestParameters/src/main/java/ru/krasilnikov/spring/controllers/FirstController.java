51acfb1fe9fe60b947c5d83684699b8d84138469
user <kavkras83@mail.ru>
2021-07-22 11:07:36 +0300

    refactoring: @RequestParam

--- SpringRequestParameters/src/main/java/ru/krasilnikov/spring/controllers/FirstController.java
+++ SpringRequestParameters/src/main/java/ru/krasilnikov/spring/controllers/FirstController.java
@@ -7,24 +7,32 @@ package ru.krasilnikov.spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
 	@GetMapping("/hello")
-	public String helloPage(HttpServletRequest request){
-		String name = request.getParameter("name");
-		String surname = request.getParameter("surname");
+//	public String helloPage(HttpServletRequest request){
+//		String name = request.getParameter("name");
+//		String surname = request.getParameter("surname");
 
+	public String helloPage(@RequestParam("name") String name,
+							@RequestParam("surname") String surname) {
 
-		System.out.println("Hello, "+ name+ " "+surname);
+		System.out.println("Hello, " + name + " " + surname);
 		return "first/hello";
 	}
 
+//	@GetMapping("/hello")
+//	public String helloPage(@RequestParam("name") String name,
+//								   @RequestParam("surname") String surname){
+//
+//		System.out.println("Hello, "+ name+ " "+surname);
+//		return "first/hello";
+//	}
+
 	@GetMapping("/goodbye")
-	public String goodByePage(){
+	public String goodByePage() {
 		return "first/goodbye";
 	}
 }
